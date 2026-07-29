1e1f5884ac02e644fc1efebb2c1ed0523c0c0339
deepakinmca <deepakinmca@gmail.com>
2026-07-29 08:59:10 +0530

    Updated

--- tests/Others/test_sync.py
+++ tests/Others/test_sync.py
@@ -6,6 +6,6 @@ def test_syncmethod():
         page = browser.new_page()
         page.goto("https://www.google.com")
         page.screenshot(path="Demo3.png")
-        page.goto("https://www.flipkart.com")
+        page.goto("https://www.flipkart.com", wait_until="domcontentloaded", timeout=10000)
         page.screenshot(path="Demo4.png")
         page.close()
