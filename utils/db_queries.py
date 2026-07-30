35354c4ec487c81149c603f25211cd319f2570dd
deepakinmca <deepakinmca@gmail.com>
2026-07-30 11:11:25 +0530

    Cleanup Files again

--- utils/db_queries.py
+++ utils/db_queries.py
@@ -1,7 +1,57 @@
-get_employee(employee_id)
+from utils.db_connection import get_db_connection
 
-employee_exists(employee_id)
 
-get_employee_by_name(first_name, last_name)
+def get_employee(employee_id):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
 
-get_all_employees()
+    query = """
+    SELECT *
+    FROM hs_hr_employee
+    WHERE employee_id = %s
+    """
+
+    cursor.execute(query, (employee_id,))
+    employee = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return employee
+
+
+def employee_exists(employee_id):
+    return get_employee(employee_id) is not None
+
+
+def get_employee_by_name(first_name, last_name):
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    query = """
+    SELECT *
+    FROM hs_hr_employee
+    WHERE emp_firstname = %s
+      AND emp_lastname = %s
+    """
+
+    cursor.execute(query, (first_name, last_name))
+    employee = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return employee
+
+
+def get_all_employees():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM hs_hr_employee")
+    employees = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return employees
